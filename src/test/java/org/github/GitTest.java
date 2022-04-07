6e1281ffb93e9a19606e3fc1c1b1f50361460ef7
Archanaradha <chellarcha@gmail.com>
2022-04-07 21:26:22 +0530

    Hello

---
+++ src/test/java/org/github/GitTest.java
@@ -0,0 +1,17 @@
+package org.github;
+
+public class GitTest {
+
+	public void Coffee() {
+		System.out.println("Coffee");
+
+	}
+
+	
+	public void Tea() {
+		System.out.println("Tea");
+
+	}
+
+	
+}
